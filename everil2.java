7e58e4269125656a6c9b0f935932a3c469e2a1c6
Averil Dwi Yokta Mauladani <averil_mauladani_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 13:54:59 +0700

    Add files via upload

---
+++ everil2.java
@@ -0,0 +1,6 @@
+public class everil2 {
+    public static void main(String[] args) {
+        System.out.println("Nama saya Averil Dwi Yokta Mauladani dari kelas X-RPL 7 yang berasal dari kota Lumajang");
+        String a = "1";
+    }
+}
