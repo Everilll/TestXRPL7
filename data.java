8a0d7c66626e6597feadb9f792f4c28ecee358a9
Averil Dwi Yokta Mauladani <averil_mauladani_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 12:21:53 +0700

    Add files via upload

---
+++ data.java
@@ -0,0 +1,14 @@
+public class data {
+    public static void main (String[] args) {
+        //tipe_data nama_var = value/isi var;
+        String nama = "averil";
+        int usia = 16;
+        String alamat = "Jl. Danau Ranau g6 b2";
+        double kelas = 10.7;
+
+        System.out.println("Nama saya : " + nama);
+        System.out.println("usia : " + usia);
+        System.out.println("alamat : " + alamat);
+        System.out.println("kelas : " + kelas);
+    }
+}
